f946ead4ff563dfdaae7811756da23416c9f331b
Somnath Dubey <93315832+somnath229@users.noreply.github.com>
2022-09-10 17:07:03 +0530

    Add files via upload

---
+++ SnakeGame/SnakeGame.java
@@ -0,0 +1,9 @@
+package SnakeGame;
+
+public class SnakeGame {
+    public static void main(String[] args) {
+//        GameFrame frame = new GameFrame(); -> if we don't plan to use frame further we can just create object without instance also
+        new GameFrame();
+
+    }
+}
